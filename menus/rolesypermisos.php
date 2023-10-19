f3d00335a1cfc36e1f5dfd6c5afd4cc0b6756ab4
Sebalfonsos <sebastian.sierrat@cecar.edu.co>
2023-10-18 22:33:24 -0500

    Added

---
+++ menus/rolesypermisos.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Blank Page</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini">
+
+
+
+
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Blank Page</h1>
+                    </div>
+
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <div class="column">
+                <div class="col-3 mb-2 ml-1">
+                    <button type="button" class="btn btn-block btn-primary btn-lg">Crear Rol</button>
+                </div>
+
+                <div class="row ml-2">
+
+                    
+                    <!-- List group -->
+                    <div class="list-group col-3" id="myList" role="tablist">
+                        <a class="list-group-item list-group-item-action active" data-toggle="list" href="#home"
+                            role="tab">Home</a>
+                        <a class="list-group-item list-group-item-action" data-toggle="list" href="#profile"
+                            role="tab">Profile</a>
+                        <a class="list-group-item list-group-item-action" data-toggle="list" href="#messages"
+                            role="tab">Messages</a>
+                        <a class="list-group-item list-group-item-action" data-toggle="list" href="#settings"
+                            role="tab">Settings</a>
+                    </div>
+
+                    <!-- Tab panes -->
+                    
+                    <div class="tab-content col-8">
+                    <label for="">Acceso a los modulos:</label>
+                        <div class="tab-pane active" id="home" role="tabpanel">
+                            <ul class="list-group">
+                                <li class="list-group-item">
+                                    Employee
+                                    <div class=" float-right">
+                                        <input id="" type="checkbox">
+                                        
+                                    </div>
+                                </li>
+                                <li class="list-group-item">
+                                    Employee
+                                    <div class=" float-right">
+                                        <input id="" type="checkbox">
+                                        
+                                    </div>
+                                </li>
+                                
+                            </ul>
+
+                        </div>
+                        <div class="tab-pane" id="profile" role="tabpanel">..2</div>
+                        <div class="tab-pane" id="messages" role="tabpanel">..3</div>
+                        <div class="tab-pane" id="settings" role="tabpanel">..4</div>
+                    </div>
+
+                </div>
+            </div>
+
+
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+    <footer class="main-footer">
+        <div class="float-right d-none d-sm-block">
+            <b>Version</b> 3.2.0
+        </div>
+        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
+        reserved.
+    </footer>
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+
+
+    <!-- jQuery -->
+    <script src="../../plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../../dist/js/adminlte.min.js"></script>
+
+</body>
+
+</html>
