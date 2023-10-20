f605a64dbc1b8c8de4590bb518c08a8a9a5f38b6
Sebalfonsos <sebastian.sierrat@cecar.edu.co>
2023-10-20 15:06:36 -0500

    simplemente magia negra, pero ya sirve

---
+++ consultasdb/roles/consultarRolesYPermisos.php
@@ -0,0 +1,83 @@
+<?php
+
+require '../consultasdb/conexion.php';
+$result = $conn->query("SELECT * FROM Roles");
+$resultPermisos = $conn->query("SELECT R.idRol, R.nombreRol, SW.idseccionWEB, SW.nombreSeccion,
+CASE WHEN RHS.Roles_idRol IS NOT NULL THEN 1 ELSE 0 END AS tieneAcceso
+FROM Roles AS R
+CROSS JOIN seccionesWEB AS SW
+LEFT JOIN Roles_has_seccionesWEB AS RHS
+ON R.idRol = RHS.Roles_idRol AND SW.idseccionWEB = RHS.seccionesWEB_idseccionWEB
+ORDER BY R.idRol, SW.idseccionWEB;");
+
+$conn->close();
+$roles = array();
+
+
+while ($row = $result->fetch_assoc()) {
+    $roles[] = array(
+        'idRol' => $row['idRol'],
+        'nombreRol' => $row['nombreRol']
+    );
+}
+
+while ($row = $resultPermisos->fetch_assoc()) {
+    $permisos[] = array(
+        'idRol' => $row['idRol'],
+        'idseccionWEB' => $row['idseccionWEB'],
+        'nombreSeccion' => $row['nombreSeccion'],
+        'tieneAcceso' => $row['tieneAcceso']
+    );
+}
+
+
+function trearRoles()
+{
+
+    global $roles;
+
+
+
+    foreach ($roles as $rol) {
+        echo '<a class="list-group-item list-group-item-action" data-toggle="list" href="#' . $rol['nombreRol'] . '"
+        role="tab">' . $rol['nombreRol'] . '</a>';
+    }
+
+
+}
+
+function traerPermisos()
+{
+    global $roles;
+    global $permisos;
+
+
+    foreach ($roles as $rol) {
+        echo '<div class="tab-pane" id="' . $rol['nombreRol'] . '" role="tabpanel">';
+        echo '<ul class="list-group">';
+        foreach ($permisos as $permiso) {
+            if ($rol['idRol'] == $permiso['idRol']) {
+                
+                if($permiso['tieneAcceso'] == 1){
+                    $tieneAcceso = "checked";
+                }else{
+                    $tieneAcceso = "";
+                }
+                echo'<li class="list-group-item">
+                        '.$permiso['nombreSeccion'].'
+                        <div class=" float-right">
+                            <input id="" type="checkbox" '.$tieneAcceso.'>
+                            
+                        </div>
+                    </li>';
+            }
+            
+        }
+
+        echo '</ul>';
+        echo '</div>';
+
+    }
+
+}
+?>
