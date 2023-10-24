ecb188d0cc6c95d56d9175b017816b4d08f2489f
Sebalfonsos <sebastian.sierrat@cecar.edu.co>
2023-10-24 09:01:54 -0500

    reservar.php

---
+++ menus/reservar.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Blank Page</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini">
+  <!-- Site wrapper -->
+  <div>
+
+
+    <!-- Content Wrapper. Contains page content -->
+    <div>
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <div class="row d-flex align-items-center">
+          <div class="text-center;" style="margin-left: 1%;">
+            <i class="nav-icon fas fa-sign-in-alt" style=" font-size: 2rem;"></i>
+          </div>
+          <h1 style="padding-left: 1%;">Reservar</h1>
+        </div>
+      </section>
+
+      <!-- Main content -->
+      <section class="content">
+
+        <div class="container-fluid">
+
+          <div class="row">
+
+            <?php
+              require '../consultasdb/recepcion.php';
+              traerHabitaciones();
+            ?>
+
+          </div>    
+          
+        </div>
+
+
+      </section>
+      <!-- /.content -->
+
+    </div>
+    <!-- /.content-wrapper -->
+
+
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+  </div>
+  <!-- ./wrapper -->
+
+  <!-- jQuery -->
+  <script src="../../plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="../../dist/js/adminlte.min.js"></script>
+
+</body>
+
+</html>
